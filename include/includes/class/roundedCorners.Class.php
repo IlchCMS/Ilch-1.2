6e9627bb025f3115083a55f2d65bbcc485bcd759
annemarie <annemarie@7d2c13f8-0d41-490c-9f91-c6373583403b>
2010-04-01 15:02:57 +0000

    added feature for borders

--- include/includes/class/roundedCorners.Class.php
+++ include/includes/class/roundedCorners.Class.php
@@ -36,6 +36,13 @@ class roundedCorner
      * @var 
      */
     protected $_radius;
+    
+    /**
+     * stores the radius of the rounded corners that needs to be created for borders
+     *
+     * @var 
+     */
+    protected $_radius_2;
 
     /**
      * stores the color of the rounded corners
@@ -52,6 +59,13 @@ class roundedCorner
     protected $_corner_image;
 
     /**
+     * stores the rounded corners that needs to be created for borders
+     *
+     * @var 
+     */
+    protected $_corner_image_2;
+
+    /**
      * stores the transparent mask for the rounded corner
      *
      * @var 
@@ -59,11 +73,25 @@ class roundedCorner
     protected $_clear_color;
     
     /**
+     * stores the transparent mask for the rounded corner that needs to be created for borders
+     *
+     * @var 
+     */
+    protected $_clear_color_2;
+    
+    /**
      * stores ... uhm ... solid colors you can rely on
      *
      * @var 
      */
     protected $_solid_color;
+    
+    /**
+     * stores ... uhm ... solid colors you can rely on that needs to be created for borders
+     *
+     * @var 
+     */
+    protected $_solid_color_2;
 
     /**
      * stores the color that get replaced with transparency
@@ -71,25 +99,66 @@ class roundedCorner
      * @var 
      */    
     protected $_transparencyColor;
+
+    /**
+     * stores the color that get replaced with transparency that needs to be created for borders
+     *
+     * @var 
+     */        
+    protected $_transparencyColor_2;
     
-    public function generateRoundedCornerImage($imageObj, $radius = '10', $backgroundcolor = NULL, $roundcorners = 'all', $savePath = '', $saveName = 'fallback', $transparencyColor = 'ff00f0')
+    /**
+     * stores the thickness of the borders
+     *
+     * @var 
+     */            
+    protected $_borderThickness;
+    
+    /**
+     * stores the color of the borders
+     *
+     * @var 
+     */        
+    protected $_borderColor;
+    
+    protected $_border = false;
+    
+    public function generateRoundedCornerImage($imageObj, $radius = '10', $backgroundcolor = NULL, $roundcorners = 'all', $border = false, $savePath = '', $saveName = 'fallback', $transparencyColor = 'ff00f0')
     {
         list( $this->_source_width, $this->_source_height, $this->_source_type ) = getimagesize( $imageObj );
         $this->_radius = $radius;
+        if($border == true) {
+            $this->_border = $border;
+            $this->_radius_2 = $radius + 5;
+            $this->_borderThickness = 0;
+            $this->_borderColor = $this->hex2rgb('ACACAC');
+        }
         $this->_transparencyColor = $transparencyColor;
         $this->_backgroundcolor = $this->getBackgroundcolor($backgroundcolor);
         $this->_roundcorners = $roundcorners;
         $this->_source_image = imagecreatefromjpeg( $imageObj );
+        $this->drawBorder();
         $this->createCornerMask();
         $this->applyRoundedCorners();
         $this->doTransparentCorners();
         imagepng( $this->_source_image, $savePath.$saveName.'.png' );
     }
     
+    private function drawBorder()
+    {
+        if($this->_borderThickness != -1 && $this->_border == true){
+            $color = imagecolorallocate($this->_source_image, $this->_borderColor[0], $this->_borderColor[1], $this->_borderColor[2]);
+            imagerectangle($this->_source_image, $this->_borderThickness, $this->_borderThickness, $this->_source_width - $this->_borderThickness, $this->_source_height - $this->_borderThickness, $color);
+            $this->_borderThickness--;
+            $this->drawBorder();
+        }
+        return false;
+    }
+    
     private function doTransparentCorners()
     {
         if ($this->_backgroundcolor == $this->_transparencyColor) {
-            $rgb_bg_color = $this->hex2rgb();
+            $rgb_bg_color = $this->hex2rgb($this->_backgroundcolor);
             $indexcolor = imagecolorallocate($this->_source_image, $rgb_bg_color[0], $rgb_bg_color[1], $rgb_bg_color[2]);
             imagefill($this->_source_image, 0, 0, $indexcolor); 
             imagecolortransparent  ($this->_source_image, $indexcolor );
@@ -114,9 +183,8 @@ class roundedCorner
         }
     }
     
-    private function hex2rgb()
+    private function hex2rgb($color)
     {
-        $color = $this->_backgroundcolor;
         if ($color[0] == '#')
             $color = substr($color, 1);
     
@@ -155,10 +223,12 @@ class roundedCorner
             hexdec( substr( $this->_backgroundcolor, 4, 2 ) )
         );
         
+    
         imagecolortransparent(
             $this->_corner_image,
             $this->_clear_color
         );
+        
         
         imagefill(
             $this->_corner_image,
@@ -175,11 +245,66 @@ class roundedCorner
             $this->_radius * 2,
             $this->_clear_color
         );
+
+        if ($this->_border) {
+            $this->_corner_image_2 = imagecreatetruecolor(
+                $this->_radius_2,
+                $this->_radius_2
+            );
+            
+            $this->_clear_color_2 = imagecolorallocate(
+                $this->_corner_image_2,
+                0,
+                0,
+                0
+            );
+            
+            $this->_solid_color_2 = imagecolorallocate(
+                $this->_corner_image_2,
+                $this->_borderColor[0],
+                $this->_borderColor[1],
+                $this->_borderColor[2]
+            );
+            
+            imagecolortransparent(
+                $this->_corner_image_2,
+                $this->_clear_color_2
+            );
+            
+            imagefill(
+                $this->_corner_image_2,
+                0,
+                0,
+                $this->_solid_color_2
+            );        
+            
+            imagefilledellipse(
+                $this->_corner_image_2,
+                $this->_radius_2,
+                $this->_radius_2,
+                $this->_radius_2 * 2,
+                $this->_radius_2 * 2,
+                $this->_clear_color_2
+            );
+        }
     }
     
     private function applyRoundedCorners()
     {
         if($this->_roundcorners == 'all' || in_array('topleft', $this->_roundcorners)){
+            
+            imagecopymerge(
+                $this->_source_image,
+                $this->_corner_image_2,
+                0,
+                0,
+                0,
+                0,
+                $this->_radius_2,
+                $this->_radius_2,
+                100
+            );
+            
             imagecopymerge(
                 $this->_source_image,
                 $this->_corner_image,
@@ -194,8 +319,22 @@ class roundedCorner
         }
         
         $this->_corner_image = imagerotate( $this->_corner_image, 90, 0 );
+        $this->_corner_image_2 = imagerotate( $this->_corner_image_2, 90, 0 );
         
         if($this->_roundcorners == 'all' || in_array('bottomleft', $this->_roundcorners)){
+            
+            imagecopymerge(
+                $this->_source_image,
+                $this->_corner_image_2,
+                0,
+                $this->_source_height - $this->_radius_2,
+                0,
+                0,
+                $this->_radius_2,
+                $this->_radius_2,
+                100
+            );
+            
             imagecopymerge(
                 $this->_source_image,
                 $this->_corner_image,
@@ -210,8 +349,22 @@ class roundedCorner
         }
         
         $this->_corner_image = imagerotate( $this->_corner_image, 90, 0 );
+        $this->_corner_image_2 = imagerotate( $this->_corner_image_2, 90, 0 );
         
         if($this->_roundcorners == 'all' || in_array('bottomright', $this->_roundcorners)){
+            
+            imagecopymerge(
+                $this->_source_image,
+                $this->_corner_image_2,
+                $this->_source_width - $this->_radius_2,
+                $this->_source_height - $this->_radius_2,
+                0,
+                0,
+                $this->_radius_2,
+                $this->_radius_2,
+                100
+            );
+
             imagecopymerge(
                 $this->_source_image,
                 $this->_corner_image,
@@ -226,8 +379,21 @@ class roundedCorner
         }
         
         $this->_corner_image = imagerotate( $this->_corner_image, 90, 0 );
+        $this->_corner_image_2 = imagerotate( $this->_corner_image_2, 90, 0 );
 
         if($this->_roundcorners == 'all' || in_array('topright', $this->_roundcorners)){        
+            imagecopymerge(
+                $this->_source_image,
+                $this->_corner_image_2,
+                $this->_source_width - $this->_radius_2,
+                0,
+                0,
+                0,
+                $this->_radius_2,
+                $this->_radius_2,
+                100
+            );
+
             imagecopymerge(
                 $this->_source_image,
                 $this->_corner_image,
