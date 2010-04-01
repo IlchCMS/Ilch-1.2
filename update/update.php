e2f188cefa742bea242e759fd19e873ca5679862
lookout <lookout@7d2c13f8-0d41-490c-9f91-c6373583403b>
2010-04-01 22:01:51 +0000

    follow up

---
+++ update/update.php
@@ -0,0 +1,36 @@
+<?php
+// update-datei für das sql. Siehe Entwicklerdokumentation
+$files = read_ext("update/revision", "php");
+
+// updatefiles bestimmen
+$updateFiles = array();
+$versions = array();
+
+foreach($files as $file) {
+	if(preg_match("/revision_[0-9]+/", $file)) {
+		$updateFiles[] = $file;
+		$parts = explode("_", $file);
+		$versionsnummer = explode(".", $parts[1]);
+		$versions[$file] = $versionsnummer[0];
+	}
+}
+// sortieren nach versionen
+asort($versions);
+
+if(!isset($allgAr["revision"])) {
+	$currentverison = 0;
+} else {
+	$currentversion = $allgAr["revision"];
+}
+
+foreach($versions as $key => $version) {
+	if($currentversion < $version) {
+		// dann müssen wir ein update machen
+		include_once("update/revision/" . $key);
+		$currentversion = $version;
+	}
+}
+
+// aktuelle version setzen
+db_query(sprintf("UPDATE `prefix_config` SET `wert` =  '%d' WHERE `schl` = 'revision';", $currentversion));
+?>
