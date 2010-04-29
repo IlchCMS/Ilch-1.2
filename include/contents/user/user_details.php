42b4b2bcc1c3534b7bb671f52708721a950ddf9c
geck0 <geck0@7d2c13f8-0d41-490c-9f91-c6373583403b>
2010-04-29 06:50:33 +0000

    Prüfen Ob User-ID vorhanden ist, falls nicht Fehlermeldung ausgeben, anstelle von PHP-Errors ;)

--- include/contents/user/user_details.php
+++ include/contents/user/user_details.php
@@ -7,40 +7,53 @@ $uid = $menu->get( 2 );
 
 $abf = 'SELECT * FROM `prefix_user` WHERE id = "' . $uid . '"';
 $erg = db_query( $abf );
-$row = db_fetch_assoc( $erg );
-
-$userpic = '';
-if ( file_exists( $row[ 'userpic' ] ) ) {
-    $userpic = '<img src="' . $row[ 'userpic' ] . '" border="0">';
+// prüfen ob ID Vorhanden ist
+$check = db_num_rows($erg);
+if ($check == 1) {
+	// User ist vorhanden
+	$row = db_fetch_assoc( $erg );
+	
+	$userpic = '';
+	if ( file_exists( $row[ 'userpic' ] ) ) {
+	    $userpic = '<img src="' . $row[ 'userpic' ] . '" border="0">';
+	}
+	
+	$regsek   = mktime( 0, 0, 0, date( 'm' ), date( 'd' ), date( 'Y' ) ) - $row[ 'regist' ];
+	$regday   = round( $regsek / 86400 );
+	$postpday = ( $regday == 0 ? 0 : round( $row[ 'posts' ] / $regday, 2 ) );
+	
+	$ar = array(
+	     'NAME' => $row[ 'name' ],
+	    'JOINED' => date( 'd M Y', $row[ 'regist' ] ),
+	    'LASTAK' => date( 'd M Y - H:i', $row[ 'llogin' ] ),
+	    'POSTS' => $row[ 'posts' ],
+	    'postpday' => $postpday,
+	    'RANG' => userrang( $row[ 'posts' ], $uid ),
+	    'AVATA' => $userpic 
+	);
+	
+	$title  = $allgAr[ 'title' ] . ' :: Users :: Details von ' . $row[ 'name' ];
+	$hmenu  = $extented_forum_menu . '<a class="smalfont" href="?user">Users</a><b> &raquo; </b> Details von ' . $row[ 'name' ] . $extented_forum_menu_sufix;
+	$design = new design( $title, $hmenu, 1 );
+	$design->header();
+	
+	$tpl = new tpl( 'user/userdetails' );
+	
+	$l = profilefields_show( $uid );
+	
+	$ar[ 'rowspan' ] = 4 + substr_count( $l, '<tr><td class="' );
+	
+	$ar[ 'profilefields' ] = $l;
+	$tpl->set_ar_out( $ar, 0 );
+	$design->footer();
+} else {
+	// User ist nicht (mehr) vorhanden
+	$title  = $allgAr[ 'title' ] . ' :: Users :: Details von ' . $row[ 'name' ];
+	$hmenu  = $extented_forum_menu . '<a class="smalfont" href="?user">Users</a><b> &raquo; </b>';
+	$design = new design( $title, $hmenu, 1 );
+	$design->header();
+	
+	echo ' FEHLER: User nicht (mehr) vorhanden';
+	$design->footer();
 }
-
-$regsek   = mktime( 0, 0, 0, date( 'm' ), date( 'd' ), date( 'Y' ) ) - $row[ 'regist' ];
-$regday   = round( $regsek / 86400 );
-$postpday = ( $regday == 0 ? 0 : round( $row[ 'posts' ] / $regday, 2 ) );
-
-$ar = array(
-     'NAME' => $row[ 'name' ],
-    'JOINED' => date( 'd M Y', $row[ 'regist' ] ),
-    'LASTAK' => date( 'd M Y - H:i', $row[ 'llogin' ] ),
-    'POSTS' => $row[ 'posts' ],
-    'postpday' => $postpday,
-    'RANG' => userrang( $row[ 'posts' ], $uid ),
-    'AVATA' => $userpic 
-);
-
-$title  = $allgAr[ 'title' ] . ' :: Users :: Details von ' . $row[ 'name' ];
-$hmenu  = $extented_forum_menu . '<a class="smalfont" href="?user">Users</a><b> &raquo; </b> Details von ' . $row[ 'name' ] . $extented_forum_menu_sufix;
-$design = new design( $title, $hmenu, 1 );
-$design->header();
-
-$tpl = new tpl( 'user/userdetails' );
-
-$l = profilefields_show( $uid );
-
-$ar[ 'rowspan' ] = 4 + substr_count( $l, '<tr><td class="' );
-
-$ar[ 'profilefields' ] = $l;
-$tpl->set_ar_out( $ar, 0 );
-$design->footer();
-
 ?>
