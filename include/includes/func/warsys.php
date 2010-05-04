9f62d743acd3f807a7a51e999c4615a5e1ce6531
geck0 <geck0@7d2c13f8-0d41-490c-9f91-c6373583403b>
2010-05-04 11:27:15 +0000

    auslesen der Team-Bilder gefixxed Funktion war Fehlerhaft und stumpfsinnig

--- include/includes/func/warsys.php
+++ include/includes/func/warsys.php
@@ -1,15 +1,15 @@
 <?php
 // Copyright by Manuel
 // Support www.ilch.de
-// codeedit by Rolf Berleth
+
 defined( 'main' ) or die( 'no direct access' );
 // ##Teambildauswahl###
 function get_teampic_ar( )
 {
     $ar = array( );
     $o  = opendir( 'include/images/teams' );
-    while ( $f = readdir( $o ) ) {
-        if ( !preg_match("/\..*/", $f) ) {
+    while ( $f = readdir( $o ))  {
+        if ( $f != "." AND $f != ".." AND $f != '.svn' AND is_file( 'include/images/teams/' . $f ) ) {
             $ar[ 'include/images/teams/' . $f ] = $f;
         }
     }
