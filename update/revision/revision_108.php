1c3664b38a3ef28a52462c4f525e68cdc6c6a632
annemarie <annemarie@7d2c13f8-0d41-490c-9f91-c6373583403b>
2010-11-21 12:52:56 +0000

    fehlerhafter query in revision #108 update script

--- update/revision/revision_108.php
+++ update/revision/revision_108.php
@@ -2,7 +2,7 @@
 /* UPDATE FÜR REVISION 108+ */
 
 // pos zu den smilies hinzufügen
-db_query("ALTER TABLE `prefix_smilies` ADD `pos` INT( 8 ) NOT NULL AUTO_INCREMENT PRIMARY KEY" );
+db_query("ALTER TABLE `prefix_smilies` ADD `pos` INT( 8 ) NOT NULL" );
 
 // initiale werte für pos
 db_query("UPDATE `prefix_smilies` SET `pos` = `id`");
