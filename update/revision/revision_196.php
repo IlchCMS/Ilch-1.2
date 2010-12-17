4c95eab1af6959626fd56060ab6a8738142ba2f4
geck0 <geck0@7d2c13f8-0d41-490c-9f91-c6373583403b>
2010-12-17 15:38:24 +0000

    bug in der update/revision_196.php behoben ( prefix__wars -> prefix_wars)

--- update/revision/revision_196.php
+++ update/revision/revision_196.php
@@ -5,7 +5,7 @@
 db_query("ALTER TABLE `prefix_smilies` ADD `pos` INT( 8 ) NOT NULL AUTO_INCREMENT PRIMARY KEY" );
 
 // initiale werte für pos
-db_query("ALTER TABLE `prefix__wars` CHANGE `wo` `wo` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL DEFAULT ''");
+db_query("ALTER TABLE `prefix_wars` CHANGE `wo` `wo` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL DEFAULT ''");
 
 $rev='196';
 $update_messages[$rev][] = 'Wars - "Wo" spalte in der datenbank auf 255 zeichen verl&auml;ngert um platz f&uuml;r adressen zu geben';
