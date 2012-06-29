4fee680608bbaa9b3f9a0a71a456778742f02e96
finke <Surf-finke@gmx.de>
2012-06-29 11:58:04 +0200

    UTF-8 eingestellt

--- include/includes/class/passwd_crypt.php
+++ include/includes/class/passwd_crypt.php
@@ -17,10 +17,10 @@ class PasswdCrypt{
 	public function __construct(){
 		mt_srand();
 		
-		if(version_compare(PHP_VERSION, '5.3.0', '<')){	//Prüfen welche Hash Funktionen Verfügbar sind. Ab 5.3 werden alle Mitgeliefert
-			if($this->hashAlgorithm == '6' && !defined('CRYPT_SHA512') $this->hashAlgoriathm = '5'; // Wenn SHA512 nicht verfügbar, versuche SHA256
-			if($this->hashAlgorithm == '5' && !defined('CRYPT_SHA256') $this->hashAlgorithm = '2a'; // Wenn SHA256 nicht verfügbar, versuche BLOWFISH
-			if($this->hashAlgorithm == '2a' && !defined('CRYPT_BLOWFISH') $this->hashAlgorithm = '1'; // Wenn BLOWFISH nicht verfügbar, nutze MD5
+		if(version_compare(PHP_VERSION, '5.3.0', '<')){	//PrÃ¼fen welche Hash Funktionen VerfÃ¼gbar sind. Ab 5.3 werden alle Mitgeliefert
+			if($this->hashAlgorithm == '6' && !defined('CRYPT_SHA512') $this->hashAlgoriathm = '5'; // Wenn SHA512 nicht verfÃ¼gbar, versuche SHA256
+			if($this->hashAlgorithm == '5' && !defined('CRYPT_SHA256') $this->hashAlgorithm = '2a'; // Wenn SHA256 nicht verfÃ¼gbar, versuche BLOWFISH
+			if($this->hashAlgorithm == '2a' && !defined('CRYPT_BLOWFISH') $this->hashAlgorithm = '1'; // Wenn BLOWFISH nicht verfÃ¼gbar, nutze MD5
 		}
 	}
 	
@@ -33,14 +33,14 @@ class PasswdCrypt{
 		}
 	  
 		if($url & self::WITH_SPECIAL_CHARACTERS){
-			$pool .= ',.-;:_#+*~!§$%&/()=?';
+			$pool .= ',.-;:_#+*~!$%&/()=?';
 		}
 	  
 		$pool = str_shuffle($pool);
 		$pool_size = strlen($pool);
 		$string ='';
 		for($i = 0;$i<$size; $i++){
-			$string .= $pool[mt_rand(0, $pool_size - 1)]; //!TODO Zufallszahlen aus /dev/random bzw /dev/urandom wenn verfügbar
+			$string .= $pool[mt_rand(0, $pool_size - 1)]; //!TODO Zufallszahlen aus /dev/random bzw /dev/urandom wenn verfÃ¼gbar
 		}
 		return $string;
 	}
