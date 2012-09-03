1b862cb0dd44c0a49889d306061acaa099f5df7e
Rocketwulf <rocketwulf@web.de>
2012-09-03 13:36:59 +0200

    Changed header output

--- include/admin/wars.php
+++ include/admin/wars.php
@@ -7,36 +7,49 @@
 defined('main') or die('no direct access');
 defined('admin') or die('only admin access');
 
-if(($menu->getA(2)=='e' AND $menu->getA(3)=='d') OR ($menu->getA(2)=='e' AND $menu->getA(3)=='a') OR $menu->getA(2) == 'l' OR $menu->getA(2) == 'm'){
-	$design = new design ( 'Admins Area', 'Admins Area', 0 );
-	$design->header();
-} else{
-	$design = new design ( 'Admins Area', 'Admins Area', 2 );
-	$design->header();
-}
-
 $um = $menu->get(1);
 
 switch ($um)
 {
     default:
-        include ('include/admin/contents/wars/default.php');
+        $datei = 'default';
+        $siteheader = 'Ilch Admin-Control-Panel :: Wars';
+        $contentheader =':: &Uuml;bersicht';
     break;
     // last wars
     case 'last':
-        include ('include/admin/contents/wars/last.php');
+        $datei = 'last';
+        $siteheader = 'Ilch Admin-Control-Panel :: Wars';
+        $contentheader =':: Last';
     break;
     // Next wars
     case 'next':
-        include ('include/admin/contents/wars/next.php');
+        $datei = 'next';
+        $siteheader = 'Ilch Admin-Control-Panel :: Wars';
+        $contentheader =':: Next';
     break;
     // Games
     case 'games':
-        include ('include/admin/contents/wars/games.php');
+        $datei = 'games';
+        $siteheader = 'Ilch Admin-Control-Panel :: Wars';
+        $contentheader =':: Games';
     break;
     case 'info':
-        include ('include/admin/contents/wars/details.php');
+        $datei = 'details';
+        $siteheader = 'Ilch Admin-Control-Panel :: Wars';
+        $contentheader =':: Details';
     break;
 }
+
+if(($menu->getA(2)=='e' AND $menu->getA(3)=='d') OR ($menu->getA(2)=='e' AND $menu->getA(3)=='a') OR $menu->getA(2) == 'l' OR $menu->getA(2) == 'm'){
+	$design = new design ( $siteheader, $contentheader, 0 );
+	$design->header();
+} else{
+	$design = new design ( $siteheader, $contentheader, 2 );
+	$design->header();
+}
+
+include_once ('include/admin/contents/wars/'.$datei.'.php');
+
 $design->footer();
 ?>
