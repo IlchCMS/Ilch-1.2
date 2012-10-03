3d8eb4fdee1acf1120b66f8791abaca38a807976
BigEasy0 <0bigeasy0@gmail.com>
2012-10-03 10:18:05 +0200

    Änderung zu Issue #10 /include/boxes/calender.php: datepicker highlighted nun ale kalendereinträge, geburtstage und wars(wars müßen nach push von rock neu implementiert werden da db-struct geändert wird.)
    für mich ist Issue #10 hiermit im content abgeschlossen, evtl. folgt es noch im Admin-Bereich, aber dafür werde ich ein neues issue anlegen und es future-release labeln

--- include/boxes/calender.php
+++ include/boxes/calender.php
@@ -1,7 +1,50 @@
+<?php
+$dates = '[ ';
+$erg = db_query('SELECT * FROM `prefix_kalender`');
+while($row = db_fetch_assoc($erg)) {
+    $year = date("Y", $row['time']);
+    $month = date("m", $row['time']) -1;
+    $day = date("d", $row['time']);
+    $dates .= 'new Date('.$year.','.$month.','.$day.'), ';
+}
+
+$erg = db_query('SELECT `name`, `gebdatum`, `recht`, `id`,
+    CASE WHEN ( MONTH(`gebdatum`) < MONTH(NOW()) ) OR ( MONTH(`gebdatum`) <= MONTH(NOW()) AND DAYOFMONTH(`gebdatum`) < DAYOFMONTH(NOW()) ) THEN
+    gebdatum + INTERVAL (YEAR(NOW()) - YEAR(`gebdatum`) + 1) YEAR
+    ELSE
+    `gebdatum` + INTERVAL (YEAR(NOW()) - YEAR(`gebdatum`)) YEAR
+    END
+    AS `gebtage`
+    FROM `prefix_user` WHERE `gebdatum` > 0000-00-00 ORDER BY `gebtage`;');
+    
+$year = date("Y");
+while($row = db_fetch_assoc($erg)) {
+    if ($row['recht'] <= '-1') {
+        $month = date("m", strtotime($row['gebdatum'])) -1;
+		    $day = date("d", strtotime($row['gebdatum']));
+		    $dates .= 'new Date('.$year.','.$month.','.$day.'), ';
+    }
+}
+
+$erg = db_query('SELECT `datime` FROM `prefix_wars`');
+
+    while($row = db_fetch_assoc($erg)) {
+        $year = date("Y", strtotime($row['datime']));
+    		$month = date("m", strtotime($row['datime'])) -1;
+    		$day = date("d", strtotime($row['datime']));
+    		$dates .= 'new Date('.$year.','.$month.','.$day.'), ';
+    }
+
+$dates .= '];';
+
+?>
+
 <script>
+var dates = <?php echo $dates ?>
+
 $(function() {
-	
-	$( "#datepicker" ).datepicker({ 
+
+	$( "#datepicker" ).datepicker({
 			autoSize: true,
 			monthNames: ['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'],
 			monthNamesShort: ['Jan','Feb','M&auml;r','Apr','Mai','Jun','Jul','Aug','Sep','Okt','Nov','Dez'],
@@ -11,6 +54,7 @@ $(function() {
 			changeYear: true,
 			firstDay: 1,
 			dateFormat: 'dd.mm.yy',
+			beforeShowDay: highlightDays,
 			autoSize: true,
 			buttonText: 'Choose',
 			yearRange: 'c-10:c+10',
@@ -21,8 +65,27 @@ $(function() {
 							location.href = loc+'?kalender-gotoDate-'+datesplit[2]+'-'+datesplit[1]+'-'+datesplit[0];
 					}
 		});
+		
+		function highlightDays(date) {
+        for (var i = 0; i < dates.length; i++) {
+              if (dates[i].getTime() == date.getTime()) {
+                             return [true, 'highlight'];
+                     }
+             }
+             return [true, ''];
+    }
 });
+
+
+
 </script>
+<style type="text/css">
+#highlight, .highlight {
+    background-color: red;
+}
+​</style>
+
+
 <noscript>
 Bitte JavaScript aktivieren
 </noscript>
